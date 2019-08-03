729519ba0919ce4e0ea8cb0fd48b35a781fcd9eb
Chris Stevenson <chris.stevenson@live.com>
2019-08-02 19:07:20 -0600

    Refactored test

--- learn-go-with-tests/hello/hello.go
+++ learn-go-with-tests/hello/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %v.", name)
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name + "."
 }
 
 func main() {
--- learn-go-with-tests/hello/hello_test.go
+++ learn-go-with-tests/hello/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris."
 
-	if got != want {
-		t.Errorf("Got: %q Want: %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("Got: %q Want: %q", got, want)
+		}
 	}
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris."
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, world.' when an empty string is supplied.", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World."
+
+		assertCorrectMessage(t, got, want)
+	})
 }
