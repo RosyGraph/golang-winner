f567c857fd491ad1561ff65b280d822b38e1df0f
Chris Stevenson <chris.stevenson@live.com>
2019-08-02 22:52:58 -0600

    Refactored to use methods and structs

--- learn-go-with-tests/structs-methods-interfaces/shapes_test.go
+++ learn-go-with-tests/structs-methods-interfaces/shapes_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -15,8 +15,18 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 100.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
 
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
