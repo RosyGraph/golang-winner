e7d549d351dc4ec3966e8ca8d0c927374fb54ad9
Chris Stevenson <chris.stevenson@live.com>
2019-08-03 16:29:06 -0600

    refactored error

--- learn-go-with-tests/maps/dictionary.go
+++ learn-go-with-tests/maps/dictionary.go
@@ -2,6 +2,8 @@ package main
 
 import "errors"
 
+var ErrNotFound = errors.New("could not find word")
+
 func Search(dictionary map[string]string, word string) string {
 	return dictionary[word]
 }
@@ -11,7 +13,7 @@ type Dictionary map[string]string
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find word")
+		return "", ErrNotFound
 	}
 	return definition, nil
 }
