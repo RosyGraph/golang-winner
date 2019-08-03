d80faf21401dbcbafc3b099680f90c62113230a0
Chris Stevenson <chris.stevenson@live.com>
2019-08-03 00:15:00 -0600

    Dealt with overdraft

--- learn-go-with-tests/pointers-errors/wallet.go
+++ learn-go-with-tests/pointers-errors/wallet.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Stringer interface {
 	String() string
@@ -20,8 +23,12 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.Balance() {
+		return errors.New("oh no")
+	}
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- learn-go-with-tests/pointers-errors/wallet_test.go
+++ learn-go-with-tests/pointers-errors/wallet_test.go
@@ -25,4 +25,16 @@ func TestHelloWorld(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Errorf("wanted an error but didn't get one")
+		}
+	})
 }
