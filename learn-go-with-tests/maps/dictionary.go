45fb3634dbbf4814c6ff8cf0034a3f5d32ba6e4a
Chris Stevenson <chris.stevenson@live.com>
2019-08-03 16:27:29 -0600

    added errors to Search() method and test

--- learn-go-with-tests/maps/dictionary.go
+++ learn-go-with-tests/maps/dictionary.go
@@ -1,11 +1,17 @@
 package main
 
+import "errors"
+
 func Search(dictionary map[string]string, word string) string {
 	return dictionary[word]
 }
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find word")
+	}
+	return definition, nil
 }
--- learn-go-with-tests/maps/dictionary_test.go
+++ learn-go-with-tests/maps/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unkown")
+		want := "could not find word"
+
+		if err == nil {
+			t.Fatal("expected an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
