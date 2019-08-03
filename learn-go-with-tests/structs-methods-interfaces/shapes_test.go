6ff936e806aee6f0974b5810da181893b7d42d0f
Chris Stevenson <chris.stevenson@live.com>
2019-08-02 23:08:23 -0600

    Refactored into table tests

--- learn-go-with-tests/structs-methods-interfaces/shapes_test.go
+++ learn-go-with-tests/structs-methods-interfaces/shapes_test.go
@@ -14,7 +14,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+
+	/* checkArea := func(t *testing.T, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
 		if got != want {
@@ -30,5 +45,5 @@ func TestArea(t *testing.T) {
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10.0}
 		checkArea(t, circle, 314.1592653589793)
-	})
+	}) */
 }
