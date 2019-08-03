3d237a9a5157e4ba33585f7581e7dd03b5837888
Chris Stevenson <chris.stevenson@live.com>
2019-08-02 23:12:39 -0600

    Implemented Triangle struct with Area method and tests

--- learn-go-with-tests/structs-methods-interfaces/shapes_test.go
+++ learn-go-with-tests/structs-methods-interfaces/shapes_test.go
@@ -20,6 +20,7 @@ func TestArea(t *testing.T) {
 	}{
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
@@ -28,22 +29,4 @@ func TestArea(t *testing.T) {
 			t.Errorf("got %.2f want %.2f", got, tt.want)
 		}
 	}
-
-	/* checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	}) */
 }
