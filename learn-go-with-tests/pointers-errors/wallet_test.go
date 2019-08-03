3b545e8017328d2f2747d96ec5ad55891d3f6894
Chris Stevenson <chris.stevenson@live.com>
2019-08-03 00:42:35 -0600

    completed error coverage with assertNoError

--- learn-go-with-tests/pointers-errors/wallet_test.go
+++ learn-go-with-tests/pointers-errors/wallet_test.go
@@ -11,10 +11,12 @@ func TestHelloWorld(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: 20}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
@@ -44,5 +46,12 @@ func assertError(t *testing.T, got error, want error) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
