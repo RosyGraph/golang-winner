6c5a58a4ef6c58fcb9c4701df93d563d9af2a4c3
Chris Stevenson <chris.stevenson@live.com>
2019-08-02 23:04:28 -0600

    Added shapes interface and tests

--- learn-go-with-tests/structs-methods-interfaces/shapes_test.go
+++ learn-go-with-tests/structs-methods-interfaces/shapes_test.go
@@ -13,23 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
 
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
