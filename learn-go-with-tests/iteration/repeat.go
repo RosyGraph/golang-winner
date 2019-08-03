d7bc48cac7ff391cc0469d7b926353a2e7dcae2a
Chris Stevenson <chris.stevenson@live.com>
2019-08-02 20:12:43 -0600

    Create sum.go and test

--- learn-go-with-tests/iteration/repeat.go
+++ learn-go-with-tests/iteration/repeat.go
@@ -1,6 +1,6 @@
 package iteration
 
-func repeat(s string, n int) string {
+func Repeat(s string, n int) string {
 	var repeated string
 	for i := 0; i < n; i++ {
 		repeated += s
--- learn-go-with-tests/iteration/repeat_test.go
+++ learn-go-with-tests/iteration/repeat_test.go
@@ -1,12 +1,26 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
-	got := repeat("a", 5)
+	got := Repeat("a", 5)
 	want := "aaaaa"
 
 	if got != want {
 		t.Errorf("got: '%s' want '%s'", got, want)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("c", 8))
+	// Output: cccccccc
 }
