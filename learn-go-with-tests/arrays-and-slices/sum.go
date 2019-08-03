25c101343f8bd03d2a000fc17ecc156cd16f6949
Chris Stevenson <chris.stevenson@live.com>
2019-08-02 20:40:44 -0600

    Add SumAll function and tests

--- learn-go-with-tests/arrays-and-slices/sum.go
+++ learn-go-with-tests/arrays-and-slices/sum.go
@@ -1,8 +1,19 @@
 package main
 
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
 	return
+}
+
+func SumAll(numberSlices ...[]int) []int {
+	lengthOfSlices := len(numberSlices)
+	sums := make([]int, lengthOfSlices)
+
+	for i, numberSlice := range numberSlices {
+		sums[i] = Sum(numberSlice)
+	}
+
+	return sums
 }
--- learn-go-with-tests/arrays-and-slices/sum_test.go
+++ learn-go-with-tests/arrays-and-slices/sum_test.go
@@ -1,13 +1,32 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	assertCorrectMessage := func(t *testing.T, got, want int, passed []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, passed)
+		}
+	}
 
-	if got != want {
-		t.Errorf("got %d want %d, given %v", got, want, numbers)
+	t.Run("collection of 3 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+
+		assertCorrectMessage(t, got, want, numbers)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
