305945c2d96df5e018a72f5c08285484b7418b5b
Chris Stevenson <chris.stevenson@live.com>
2019-08-05 11:32:54 -0600

    added Grid.FromString() and test

--- tic-tac-toe/tictactoe_test.go
+++ tic-tac-toe/tictactoe_test.go
@@ -6,17 +6,54 @@ import (
 )
 
 func TestPrintState(t *testing.T) {
-	buffer := bytes.Buffer{}
 
-	grid := Grid{
-		{"X", "O", "X"},
-		{" ", " ", " "},
-		{" ", " ", " "},
+	stateTests := []struct {
+		name string
+		grid Grid
+		want string
+	}{
+		{
+			name: "some taken",
+			grid: Grid{
+				{" ", "X", "X"},
+				{"O", " ", "O"},
+				{"X", "X", "O"},
+			},
+			want: "---------\n" +
+				"|   X X |\n" +
+				"| O   O |\n" +
+				"| X X O |\n" +
+				"---------\n",
+		},
 	}
-	grid.PrintState(&buffer)
 
+	for _, tt := range stateTests {
+		t.Run(tt.name, func(t *testing.T) {
+			buffer := bytes.Buffer{}
+			tt.grid.PrintState(&buffer)
+			got := buffer.String()
+			assertString(t, got, tt.want)
+		})
+	}
+}
+
+func TestFromString(t *testing.T) {
+	cells := "\"O OXXO XX\""
+	grid := Grid{}
+	grid.FromString(cells)
+	buffer := bytes.Buffer{}
+	grid.PrintState(&buffer)
 	got := buffer.String()
-	want := "X O X \n      \n      \n"
+	want := "---------\n" +
+		"| O   O |\n" +
+		"| X X O |\n" +
+		"|   X X |\n" +
+		"---------\n"
+	assertString(t, got, want)
+}
+
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
 
 	if want != got {
 		t.Errorf("got %q want %q", got, want)
