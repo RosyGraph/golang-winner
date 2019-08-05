f3f6ac8e4a94671821bc446ee75314b12295659a
Chris Stevenson <chris.stevenson@live.com>
2019-08-04 22:14:43 -0600

    added Greet() and test

---
+++ tic-tac-toe/tictactoe_test.go
@@ -0,0 +1,14 @@
+package tictactoe
+
+import "testing"
+
+func TestGrid(t *testing.T) {
+	// grid := Grid {
+	// string[3]{"X", "O", "X"},
+	// string[3]{" ", " ", " "},
+	// string[3]{" ", " ", " "},
+	// }
+
+	want := nil
+	got := nil
+}
