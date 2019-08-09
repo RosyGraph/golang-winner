357928d4c8c590ad802adfa9a7d004a13d835bec
Chris Stevenson <chris.stevenson@live.com>
2019-08-09 14:34:14 -0600

    added URL Racer function

---
+++ learn-go-with-tests/select/racer_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestHelloWorld(t *testing.T) {
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(10 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
