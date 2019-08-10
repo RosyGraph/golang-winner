5cea314e729778d0e240fd10f1f76ea5d9fce2e6
Chris Stevenson <chris.stevenson@live.com>
2019-08-09 19:54:38 -0600

    added tests up to 10

--- learn-go-with-tests/roman-numerals/roman.go
+++ learn-go-with-tests/roman-numerals/roman.go
@@ -3,7 +3,25 @@ package main
 import "fmt"
 
 func IntToRoman(num int) (ans string) {
-	for ; num > 0; num-- {
+	if num == 4 {
+		ans = "IV"
+	}
+
+	if num == 9 {
+		ans = "IX"
+	}
+
+	if num > 9 && num < 14 {
+		ans = "X"
+		num -= 10
+	}
+
+	if num > 4 && num < 9 {
+		ans = "V"
+		num -= 5
+	}
+
+	for ; num > 0 && num < 4; num-- {
 		ans += "I"
 	}
 	return
--- learn-go-with-tests/roman-numerals/roman_test.go
+++ learn-go-with-tests/roman-numerals/roman_test.go
@@ -4,18 +4,25 @@ import "testing"
 
 func TestIntToRoman(t *testing.T) {
 	cases := []struct {
-		integer  int
-		romanNum string
+		integer int
+		roman   string
 	}{
-		{integer: 1, romanNum: "I"},
-		{integer: 2, romanNum: "II"},
-		{integer: 3, romanNum: "III"},
+		{integer: 1, roman: "I"},
+		{integer: 2, roman: "II"},
+		{integer: 3, roman: "III"},
+		{integer: 4, roman: "IV"},
+		{integer: 5, roman: "V"},
+		{integer: 6, roman: "VI"},
+		{integer: 7, roman: "VII"},
+		{integer: 8, roman: "VIII"},
+		{integer: 9, roman: "IX"},
+		{integer: 10, roman: "X"},
 	}
 
 	for _, tt := range cases {
 		got := IntToRoman(tt.integer)
-		if got != tt.romanNum {
-			t.Errorf("got %s want %s given %d", got, tt.romanNum, tt.integer)
+		if got != tt.roman {
+			t.Errorf("got %s want %s given %d", got, tt.roman, tt.integer)
 		}
 	}
 }
