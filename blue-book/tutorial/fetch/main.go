b7b06e8cead997d745193dd5b707db17e63f18ad
Chris Stevenson <chris.stevenson@live.com>
2019-08-13 11:14:37 -0600

    refactored

--- blue-book/tutorial/fetch/main.go
+++ blue-book/tutorial/fetch/main.go
@@ -9,20 +9,23 @@ import (
 	"strings"
 )
 
+const httpPrefix = "http://"
+
 func main() {
 	for _, url := range os.Args[1:] {
-		if !strings.HasPrefix(url, "http://") {
-			url = "http://" + url
+		if !strings.HasPrefix(url, httpPrefix) {
+			url = httpPrefix + url
 		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+		fmt.Fprintf(os.Stdout, "fetch: reading %s: %v\n", url, resp.Status)
 		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %vs\n", url, err)
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
 	}
