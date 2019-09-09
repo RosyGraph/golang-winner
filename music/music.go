ed300fce2708d94251e581a105150aaf2a18cf0e
Chris Stevenson <chris.stevenson@live.com>
2019-09-09 14:10:00 -0600

    add note and interval structs

--- music/music.go
+++ music/music.go
@@ -4,6 +4,27 @@ const Sharp = "♯"
 const Flat = "♭"
 const Dblsharp = "𝄪"
 const Dblflat = "𝄫"
+const Natural = "♮"
+
+var NoteValues = map[string]int{
+	"A": 0,
+	"B": 2,
+	"C": 3,
+	"D": 5,
+	"E": 7,
+	"F": 8,
+	"G": 10,
+}
+
+var Alphabet = map[string]int{
+	"A": 0,
+	"B": 1,
+	"C": 2,
+	"D": 3,
+	"E": 4,
+	"F": 5,
+	"G": 6,
+}
 
 type Note struct {
 	name       string
@@ -11,5 +32,31 @@ type Note struct {
 }
 
 func NoteFromString(s string) Note {
-	return Note{"A", Sharp}
+	r := []rune(s)
+	var accidental string
+	name := string(r[0])
+
+	if len(r) > 1 {
+		accidental = string(r[1])
+	} else {
+		accidental = Natural
+	}
+	return Note{name, accidental}
+}
+
+type Interval struct {
+	quality  string
+	quantity int
+}
+
+func AscendingInterval(r, i Note) Interval {
+	var quantity int
+	/* var quality string */
+	rval := Alphabet[r.name]
+	ival := Alphabet[i.name]
+	if rval > ival {
+		ival += 8
+	}
+	quantity = ival - rval + 1
+	return Interval{"major", quantity}
 }
--- music/music_test.go
+++ music/music_test.go
@@ -3,10 +3,40 @@ package music
 import "testing"
 
 func TestNoteFromString(t *testing.T) {
-	got := NoteFromString("A" + Sharp)
-	want := Note{"A", Sharp}
-
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
+	tc := []struct {
+		name string
+		s    string
+		want Note
+	}{
+		{name: "A sharp", s: "A" + Sharp, want: Note{"A", Sharp}},
+		{name: "A natural", s: "A", want: Note{"A", Natural}},
 	}
+	for _, c := range tc {
+		t.Run(c.name, func(t *testing.T) {
+			got := NoteFromString(c.s)
+
+			if got != c.want {
+				t.Errorf("got %v want %v", got, c.want)
+			}
+		})
+	}
+}
+
+func TestAscendingInterval(t *testing.T) {
+	t.Run("major 3rd", func(t *testing.T) {
+		got := AscendingInterval(Note{"A", Natural}, Note{"C", Sharp})
+		want := Interval{"major", 3}
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("minor 3rd", func(t *testing.T) {
+		got := AscendingInterval(Note{"A", Natural}, Note{"C", Sharp})
+		want := Interval{"minor", 3}
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
