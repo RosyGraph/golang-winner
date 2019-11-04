024c179bfa539194086cbde9131a82f8fbe19594
Chris Stevenson <chris.stevenson@live.com>
2019-11-03 21:22:46 -0700

    finished hourglass function

--- kata/unsolved/matrix/matrix.go
+++ kata/unsolved/matrix/matrix.go
@@ -3,6 +3,21 @@ Calculates highest 'hourglass' sum in a given 2d array
 */
 package main
 
-func HourglassSum(a [][]int32) int32 {
-	return 19
+import "math"
+
+func hourglassSum(a [][]int32) int32 {
+	var max int32 = math.MinInt32
+	for y := 1; y < len(a)-1; y++ {
+		for x := 1; x < len(a[0])-1; x++ {
+			if s := sumGlass(a, x, y); s > max {
+				max = s
+			}
+		}
+	}
+	return max
+}
+
+func sumGlass(a [][]int32, x, y int) int32 {
+	return a[y-1][x-1] + a[y-1][x] + a[y-1][x+1] +
+		a[y][x] + a[y+1][x-1] + a[y+1][x] + a[y+1][x+1]
 }
--- kata/unsolved/matrix/matrix_test.go
+++ kata/unsolved/matrix/matrix_test.go
@@ -2,7 +2,46 @@ package main
 
 import "testing"
 
-func TestHourglassSum(t *testing.T) {
+func TesthourglassSum(t *testing.T) {
+	tc := []struct {
+		n    string
+		a    [][]int32
+		want int32
+	}{
+		{
+			n: "positive numbers",
+			a: [][]int32{
+				{1, 1, 1, 0, 0, 0},
+				{0, 1, 0, 0, 0, 0},
+				{1, 1, 1, 0, 0, 0},
+				{0, 0, 2, 4, 4, 0},
+				{0, 0, 0, 2, 0, 0},
+				{0, 0, 1, 2, 4, 0}},
+			want: 19,
+		},
+		{
+			n: "positive numbers",
+			a: [][]int32{
+				{1, 1, 1, 0, 0, 0},
+				{0, 1, 0, 0, 0, 0},
+				{1, 1, 1, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0}},
+			want: 7,
+		},
+	}
+	for _, c := range tc {
+		t.Run(c.n, func(t *testing.T) {
+			got := hourglassSum(c.a)
+			if got != c.want {
+				t.Errorf("got %d want %d", got, c.want)
+			}
+		})
+	}
+}
+
+func TestSumGlass(t *testing.T) {
 	a := [][]int32{
 		{1, 1, 1, 0, 0, 0},
 		{0, 1, 0, 0, 0, 0},
@@ -11,8 +50,8 @@ func TestHourglassSum(t *testing.T) {
 		{0, 0, 0, 2, 0, 0},
 		{0, 0, 1, 2, 4, 0},
 	}
-	got := HourglassSum(a)
-	want := int32(19)
+	got := sumGlass(a, 1, 1)
+	want := int32(7)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
