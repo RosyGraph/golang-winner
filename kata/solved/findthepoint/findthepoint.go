086690778475a014dfdccf5be73fcd5604a31d01
Chris Stevenson <chris.stevenson@live.com>
2019-11-19 09:00:54 -0700

    solve findthepoint kata

--- kata/solved/findthepoint/findthepoint.go
+++ kata/solved/findthepoint/findthepoint.go
@@ -7,17 +7,19 @@ import (
 
 func findPoint(px int32, py int32, qx int32, qy int32) []int32 {
 	pt := []int32{0, 0}
-	d := math.Sqrt(float64(qx-px)*float64(qx-px) + float64(qy-py)*float64(qy-py))
+	dx := int32(math.Sqrt(float64(px-qx) * float64(px-qx)))
+	dy := int32(math.Sqrt(float64(py-qy) * float64(py-qy)))
+
 	if px < qx {
-		pt[0] = qx + int32(d)
+		pt[0] = qx + dx
 	} else {
-		pt[0] = qx - int32(d)
+		pt[0] = qx - dx
 	}
 
 	if py < qy {
-		pt[1] = qy + int32(d)
+		pt[1] = qy + dy
 	} else {
-		pt[1] = qy - int32(d)
+		pt[1] = qy - dy
 	}
 
 	return pt
