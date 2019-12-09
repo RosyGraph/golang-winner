93fb737ab614cc134ce083ab3315c4ce8677c615
Chris Stevenson <chris.stevenson@live.com>
2019-12-09 10:21:12 -0700

    realize papersquares is not solved TODO: handle large integer values

---
+++ kata/unsolved/papersquares/papersquares.go
@@ -0,0 +1,5 @@
+package main
+
+func solve(n, m int32) uint64 {
+	return uint64(n*m - 1)
+}
--- kata/unsolved/papersquares/papersquares_test.go
+++ kata/unsolved/papersquares/papersquares_test.go
@@ -6,12 +6,15 @@ func TestSolve(t *testing.T) {
 	tc := []struct {
 		name string
 		n, m int32
-		want int64
+		want uint64
 	}{
 		{name: "3 1", n: 3, m: 1, want: 2},
 		{name: "3 2", n: 3, m: 2, want: 5},
+		{name: "2 3", n: 2, m: 3, want: 5},
 		{name: "6 6", n: 6, m: 6, want: 35},
 		{name: "12 12", n: 12, m: 12, want: 143},
+		{name: "316925480 472908316", n: 316925480, m: 472908316, want: 149876695044291679},
+		{name: "689715240 759842301", n: 689715240, m: 759842301, want: 524074814996367239},
 	}
 
 	for _, c := range tc {
