2c5c18994234e16c6a3f2a8280e761d0e27fc090
Chris Stevenson <chris.stevenson@live.com>
2019-12-24 16:36:54 -0700

    so close

--- kata/unsolved/reverse-game/reverse.go
+++ kata/unsolved/reverse-game/reverse.go
@@ -1,5 +1,25 @@
 package kata
 
 func ReverseGame(n, k int32) int32 {
-	return 2
+	b := reverse(newBalls(n))
+	for i := int32(0); i < n-1; i++ {
+		b = append(b[:i], reverse(b[i:])...)
+	}
+	return b[k]
+}
+
+func newBalls(n int32) []int32 {
+	res := make([]int32, n)
+	for i := int32(0); i < n; i++ {
+		res[i] = i
+	}
+	return res
+}
+
+func reverse(n []int32) []int32 {
+	if len(n) < 2 {
+		return n
+	}
+
+	return append(reverse(n[1:]), n[0])
 }
--- kata/unsolved/reverse-game/reverse_test.go
+++ kata/unsolved/reverse-game/reverse_test.go
@@ -1,6 +1,9 @@
 package kata
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestReverseGame(t *testing.T) {
 	tc := []struct {
@@ -9,6 +12,10 @@ func TestReverseGame(t *testing.T) {
 		want int32
 	}{
 		{name: "3 1", n: 3, k: 1, want: 2},
+		{name: "5 2", n: 5, k: 2, want: 4},
+		{name: "11 1", n: 11, k: 1, want: 3},
+		{name: "3 1", n: 3, k: 1, want: 2},
+		{name: "9 0", n: 9, k: 0, want: 1},
 	}
 
 	for _, c := range tc {
@@ -18,5 +25,38 @@ func TestReverseGame(t *testing.T) {
 				t.Errorf("give %s: got %d want %d", c.name, got, c.want)
 			}
 		})
+	}
+}
+
+func TestReverse(t *testing.T) {
+	got := reverse([]int32{0, 1, 2})
+	want := []int32{2, 1, 0}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestNewBalls(t *testing.T) {
+	tc := []struct {
+		name string
+		n    int32
+		want []int32
+	}{
+		{name: "3", n: 3, want: []int32{0, 1, 2}},
+		{name: "5", n: 5, want: []int32{0, 1, 2, 3, 4}},
+	}
+	for _, c := range tc {
+		t.Run(c.name, func(t *testing.T) {
+			got := newBalls(c.n)
+			if !reflect.DeepEqual(got, c.want) {
+				t.Errorf("given %s: got %v want %v", c.name, got, c.want)
+			}
+		})
+	}
+	got := newBalls(3)
+	want := []int32{0, 1, 2}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
