3095f7d5f6dddb332dc39eaf9db8d74dd4721229
Chris Stevenson <chris.stevenson@live.com>
2019-12-24 15:33:15 -0700

    close in on passing tests

--- kata/unsolved/primefactors/prime.go
+++ kata/unsolved/primefactors/prime.go
@@ -1,15 +1,48 @@
 package prime
 
-import "math/big"
+import (
+	"math"
+	"math/big"
+)
 
 func primeCount(n int64) int32 {
 	if n < 2 {
 		return 0
 	}
-	z := big.NewInt(n)
-	var p int64
-
-	for p := int64(2); p < n/2; p++ {
-
+	if isPrime(n) {
+		return 1
 	}
+
+	var count int32
+
+	if n%2 == 0 {
+		count++
+	}
+
+	for n%2 == 0 {
+		n /= 2
+	}
+
+	nroot := int64(math.Sqrt(float64(n)))
+	for i := int64(3); i < nroot; {
+		if n%i == 0 && isPrime(i) {
+			count++
+			n /= i
+		} else {
+			i += 2
+		}
+	}
+
+	if n > 2 && isPrime(n) {
+		count++
+	}
+
+	return count
+}
+
+func isPrime(n int64) bool {
+	x := new(big.Int)
+	x.SetInt64(n)
+
+	return x.ProbablyPrime(1)
 }
--- kata/unsolved/primefactors/prime_test.go
+++ kata/unsolved/primefactors/prime_test.go
@@ -8,18 +8,41 @@ type TestCase struct {
 	want int32
 }
 
+func TestIsPrime(t *testing.T) {
+	tc := []struct {
+		name string
+		n    int64
+		want bool
+	}{
+		{name: "13", n: 13, want: true},
+		{name: "3", n: 3, want: true},
+		{name: "6", n: 6, want: false},
+		{name: "1", n: 1, want: false},
+	}
+	for _, c := range tc {
+		t.Run(c.name, func(t *testing.T) {
+			got := isPrime(c.n)
+			if got != c.want {
+				t.Errorf("give %s: got %v want %v", c.name, got, c.want)
+			}
+		})
+	}
+}
+
 func TestPrimeCount(t *testing.T) {
 	tc := []TestCase{
 		{name: "1", n: 1, want: 0},
 		{name: "2", n: 2, want: 1},
 		{name: "3", n: 3, want: 1},
 		{name: "500", n: 500, want: 4},
+		{name: "5000", n: 5000, want: 5},
+		{name: "10000000000", n: 10000000000, want: 10},
 	}
 
 	check := func(t *testing.T, c TestCase) {
 		got := primeCount(c.n)
 		if got != c.want {
-			t.Errorf("got %d want %d", got, c.want)
+			t.Errorf("given %s: got %d want %d", c.name, got, c.want)
 		}
 	}
 
