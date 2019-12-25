0a6dce3e132f55bd9c7d652dd944a17b610695bd
Chris Stevenson <chris.stevenson@live.com>
2019-12-24 18:51:42 -0700

    add collatz experiment

---
+++ collatz/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func collatz(n int) int {
+	var counter int
+
+	for n > 1 {
+		if n%2 != 0 {
+			n = 3*n + 1
+			counter++
+		}
+		counter++
+		n /= 2
+	}
+	return counter
+}
+
+func main() {
+	n, _ := strconv.Atoi(os.Args[1])
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d: %d steps\n", i, collatz(i))
+	}
+}
