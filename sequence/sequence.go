92653b31c3476778f34854ab3b5a9893863594da
Chris Stevenson <chris.stevenson@live.com>
2020-02-04 12:42:41 -0700

    rename to sequence.go

---
+++ sequence/sequence.go
@@ -0,0 +1,5 @@
+package sequence
+
+func NthArithmetic(n, n1, d int) int {
+	return n1 + (n-1)*d
+}
---
+++ sequence/sequence_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+// 3, 0, -3, -6, -9, -12, -15, -18, -21, -24
+func TestNthArithmetic(t *testing.T) {
+	tc := []struct {
+		n    int
+		n1   int
+		d    int
+		want int
+	}{
+		{n: 1, n1: 1, d: 1, want: 1},
+		{n: 2, n1: 0, d: 1, want: 1},
+		{n: 5, n1: 3, d: 2, want: 11},
+		{n: 10, n1: 3, d: -3, want: -24},
+	}
+
+	for _, c := range tc {
+		t.Run("NthArithmetic", func(t *testing.T) {
+			got := NthArithmetic(c.n, c.n1, c.d)
+
+			if got != c.want {
+				t.Errorf("NthArithmetic(%d, %d, %d) got %d want %d",
+					c.n, c.n1, c.d, got, c.want)
+			}
+		})
+	}
+}
